3d48d9f932ee9cbcbd6075fb1903a7d3fee47c0f
SiGuy19 <silaschammond@gmail.com>
2022-11-01 18:47:50 -0700

    start
    start of project

---
+++ Player.cs
@@ -0,0 +1,36 @@
+using Raylib_cs;
+using System.Numerics;
+
+namespace Greed
+{
+    class Player
+    {
+        Vector2 playerPosition = new Vector2(300, 250);
+
+        public void movePlayer()
+        {
+            
+            var playerSpeed = 4;
+            int playerRadius = 10;
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+                    playerPosition.X += playerSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+                    playerPosition.X -= playerSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP)) {
+                    playerPosition.Y -= playerSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN)) {
+                    playerPosition.Y  += playerSpeed;
+                }
+
+                Raylib.DrawCircleV(playerPosition, playerRadius, Color.BLUE);
+            
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Greed
+{
+    class Board
+    {
+        static Player player = new Player();
+
+        public static void Main()
+        {
+            var screenHeight = 300;
+            var screenWidth = 500;
+            Raylib.InitWindow(screenHeight, screenWidth, "Greed");
+            Raylib.SetTargetFPS(60);
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+                player.movePlayer();
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
